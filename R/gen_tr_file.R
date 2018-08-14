421211e58a97f0ccaf948b410cd9ded2cff63ddb
rexon1992 <rexon1992@gmil.com>
2018-08-14 11:23:34 -0400

    read_me

---
+++ R/gen_tr_file.R
@@ -0,0 +1,14 @@
+#' Generate transition file
+#'
+#' This function generates and writes a file into the working directory with all the indicators avaliable in a dataset, its units and an empty column called "Default Indicator Name" to match model indicators to default indicators and writes. After updating the file it can then be used as an input to the transition function to replace model indicator names with default indicator names.
+#' @param d_file A string containing name of the data file excluding the extension
+#'
+gen_tr_file<-function(d_file){
+  data<-read.csv(paste(d_file,".csv",sep=""),header=T,check.names=F,stringsAsFactors=F)
+  indicators<-unique(data$`Model Indicator Name`)
+  units<-data$`Unit of Entry`[match(indicators,data$`Model Indicator Name`)]
+  default_name<-rep("",length(indicators))
+  tr<-data.frame(indicators,units,default_name)
+  colnames(tr)<-c("Model Indicator Name","Unit of Entry","Default Indicator Name")
+  write.csv(tr,paste(d_file,"tr.csv",sep="_"),row.names=F,na="")
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+pathways
+========
+
+The goal of pathways is to ...
+
+Installation
+------------
+
+You can install pathways from github with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("rexon1992/pathways")
+```
+
+Example
+-------
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+## basic example code
+```
