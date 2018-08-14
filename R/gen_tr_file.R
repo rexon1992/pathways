4dc95b63183807b2f345be3d3dce48043e61f7b0
rexon1992 <rexon1992@gmil.com>
2018-08-14 16:15:26 -0400

    gen_tr

--- R/gen_tr_file.R
+++ R/gen_tr_file.R
@@ -2,7 +2,8 @@
 #'
 #' This function generates and writes a file into the working directory with all the indicators avaliable in a dataset, its units and an empty column called "Default Indicator Name" to match model indicators to default indicators and writes. After updating the file it can then be used as an input to the transition function to replace model indicator names with default indicator names.
 #' @param d_file A string containing name of the data file excluding the extension
-#'
+#' @export
+
 gen_tr_file<-function(d_file){
   data<-read.csv(paste(d_file,".csv",sep=""),header=T,check.names=F,stringsAsFactors=F)
   indicators<-unique(data$`Model Indicator Name`)
